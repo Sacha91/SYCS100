788cb1b65f3f6e5face2e6c2d64ddbec76508973
Sacha Alexander <sacha.alex1611.sa@gmail.com>
2013-11-19 20:27:07 -0500

    added Sacha's bsearch.py code
    I added my code (Sacha) to our group file.

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,25 @@
 #Eboni
 #Ashanti
 #Tyler
+
+
+sList = ['Appricot', 'Brown', 'Green', 'Purple', 'Sun', 'Teal', 'Yellow']  
+def bSearch():
+   hi = len(sList)- 1
+   lo = sList[0]
+   found = False
+   desiredElement = sList[4]
+   while lo < hi and found == False:
+        halfElement = (lo+hi)//2
+        if sList[halfElement] < desiredElement:
+           lo = halfElement + 1
+        elif sList[halfElement] > desiredElement:
+           hi = halfElement - 1
+        else:
+           found = True
+   if found:
+         print 'found name'
+   else:
+         print '-1'
+bSearch ()
+        
