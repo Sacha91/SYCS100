d47f4305631130a3c6bbeff8132cd8920a812a7e
EboniBrown <eboni.brown@bison.howard.edu>
2013-11-20 21:27:43 -0500

    Added code to the file
    I (Eboni) added my code into the file

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,41 @@
 #Eboni
 #Ashanti
 #Tyler
+
+#Eboni's code start's here
+
+def bsearch (itemB, bList):
+    lo = 0
+    high = len(bList) - 1
+   
+    while lo <= high:
+        mid = (lo + high)//2
+
+        if bList[mid] == itemB:
+            return mid
+        elif bList[mid] < itemB:
+            lo = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+
+
+
+   
+        
+            
+    
+    
+
+                 
+    
+  
+
+
+
+    
+    
+   
+
