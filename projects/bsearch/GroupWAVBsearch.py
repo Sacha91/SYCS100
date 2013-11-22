d29960c245272e6878f76cb6608d34829617d47c
AshantiMills <ashanti.mills@bison.howard.edu>
2013-11-22 02:24:31 -0500

    Ashanti's BSearch Code
    If list is null, it skips to the end and prints -1. Otherwise, the bsearch is standard. Originally, the while loop would continue forever if x was not in the list, but I made it so that the while loop can't iterate more than [a] times, a being the length of the list.

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -23,7 +23,28 @@ def bsearch (itemB, bList):
     return -1
 
 
+#Ashanti's code starts here
 
+def bsearch(list,x):
+	if list != None:
+		hi = len(list)
+		lo = 0
+		n = (hi+lo)/2
+		a = 0
+		while list[n] != x and a < len(list):
+			if list[n] < x:
+				lo = n
+			elif list[n] > x:
+				hi = n
+			n = (hi+lo)/2
+			a += 1
+		
+		if list[n] == x:
+			print n
+		else:
+			print -1
+	else:
+		print -1	
 
 
    
