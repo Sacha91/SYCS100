36366a821906aeb395c0bf36c9a2b30038bcc67c
2wavy <tyler.jenkins@bison.howard.edu>
2013-11-22 19:28:22 -0500

    GroupWAVBSEARCH.py
    I added my perfected binary search.

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -94,12 +94,31 @@ def bSearch():
          return halfElement
    else:
          return -1
+#Tyler's code starts here
+
    
-        
+        waveList = [3,4,6,8,9]
+
+
+def binarySearch(MyPurpose, waveList):
+    Discovery = False
+    bottom = 0
+    top = len(waveList) - 1
+    if bottom == top:
+        return -1
+    while bottom<=top and not Discovery:
+        mid = (bottom+top)//2
+        if waveList[mid]==MyPurpose:
+            Discovery = True
+        elif waveList[mid] < MyPurpose:
+            bottom = mid + 1
+        else:
+            top = mid - 1
+    return Discovery
+
+    print binarySearch(MyPurpose, waveList)
             
     
-    
-
                  
     
   
